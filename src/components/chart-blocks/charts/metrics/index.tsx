7b15a10ca4f475c930bd6714f3a5ce63e000c63e
Theodoore samba <theodorebinda@gmail.com>
2026-03-20 10:00:30 +0100

    Ajout du module X

--- src/components/chart-blocks/charts/metrics/index.tsx
+++ src/components/chart-blocks/charts/metrics/index.tsx
@@ -6,7 +6,7 @@ export default function Metrics() {
   return (
     <Container className="grid grid-cols-1 gap-y-6 border-b border-border py-4 phone:grid-cols-2 laptop:grid-cols-4">
       {metrics.map((metric) => (
-        <MetricCard key={metric.title} {...metric} />
+        // <MetricCard key={metric.title} {...metric} />
       ))}
     </Container>
   );
