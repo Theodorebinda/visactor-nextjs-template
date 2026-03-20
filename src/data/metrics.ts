5092a4d509eac670902f9a8ed468f64af4db91ec
Theodoore samba <theodorebinda@gmail.com>
2026-03-20 10:00:30 +0100

    Ajout du module X

--- src/data/metrics.ts
+++ src/data/metrics.ts
@@ -13,10 +13,10 @@ export const metrics = [
     title: "Resolved Tickets",
     value: "18,208",
     change: 0.08,
-  },
-  {
-    title: "Average First Time Reply",
-    value: "12:01 min",
-    change: 0.08,
-  },
-];
+//   },
+//   {
+//     title: "Average First Time Reply",
+//     value: "12:01 min",
+//     change: 0.08,
+//   },
+// ];
