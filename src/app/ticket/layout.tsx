ee5a74bbbe783ff6d96e92d233f95cdbaacd1847
Theodoore samba <theodorebinda@gmail.com>
2026-03-28 10:00:30 +0100

    Ajout du module

--- src/app/ticket/layout.tsx
+++ src/app/ticket/layout.tsx
@@ -1,17 +1,17 @@
-import Container from "@/components/container";
-import { TopNav } from "@/components/nav";
+// import Container from "@/components/container";
+// import { TopNav } from "@/components/nav";
 
-export default function TicketLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <>
-      <TopNav title="Ticket" />
-      <main>
-        <Container>{children}</Container>
-      </main>
-    </>
-  );
-}
+// export default function TicketLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <>
+//       <TopNav title="Ticket" />
+//       <main>
+//         <Container>{children}</Container>
+//       </main>
+//     </>
+//   );
+// }
