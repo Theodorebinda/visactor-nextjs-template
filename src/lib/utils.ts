32d0954da6cb42c81a99d2c35037eb8f4daca4f5
Theodoore samba <theodorebinda@gmail.com>
2026-03-29 10:00:30 +0100

    Ajout du module

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,14 @@
-// import { type ClassValue, clsx } from "clsx";
-// import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-// export function cn(...inputs: ClassValue[]) {
-//   return twMerge(clsx(inputs));
-// }
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 
-// export function addThousandsSeparator(num: number) {
-//   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-// }
+export function addThousandsSeparator(num: number) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
-// export function numberToPercentage(num: number) {
-//   return `${num * 100}%`;
-// }
+export function numberToPercentage(num: number) {
+  return `${num * 100}%`;
+}
