df4033abc0f83342a59594fe8eb09a2f0281cb74
Theodoore samba <theodorebinda@gmail.com>
2026-03-30 10:00:30 +0100

    Ajout du module

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,17 +1,17 @@
-// import type { SVGProps } from "react";
+import type { SVGProps } from "react";
 
-// export type IconSvgProps = SVGProps<SVGSVGElement> & {
-//   size?: number;
-// };
+export type IconSvgProps = SVGProps<SVGSVGElement> & {
+  size?: number;
+};
+
+export type TicketMetric = {
+  date: string;
+  type: "created" | "resolved";
+  count: number;
+};
 
 // export type TicketMetric = {
 //   date: string;
 //   type: "created" | "resolved";
 //   count: number;
 // };
-
-// // export type TicketMetric = {
-// //   date: string;
-// //   type: "created" | "resolved";
-// //   count: number;
-// // };
