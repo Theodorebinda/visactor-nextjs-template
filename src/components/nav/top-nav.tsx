1e288d22cd25807ffb3239355f3df884c699d5db
Theodoore samba <theodorebinda@gmail.com>
2026-04-21 09:42:43 +0100

    Ajout du module X

--- src/components/nav/top-nav.tsx
+++ src/components/nav/top-nav.tsx
@@ -1,13 +1,13 @@
-// "use client";
+"use client";
 
-// import Container from "../container";
-// import { ThemeToggle } from "../theme-toggle";
+import Container from "../container";
+import { ThemeToggle } from "../theme-toggle";
 
-// export default function TopNav({ title }: { title: string }) {
-//   return (
-//     <Container className="flex h-16 items-center justify-between border-b border-border">
-//       <h1 className="text-2xl font-medium">{title}</h1>
-//       <ThemeToggle />
-//     </Container>
-//   );
-// }
+export default function TopNav({ title }: { title: string }) {
+  return (
+    <Container className="flex h-16 items-center justify-between border-b border-border">
+      <h1 className="text-2xl font-medium">{title}</h1>
+      <ThemeToggle />
+    </Container>
+  );
+}
